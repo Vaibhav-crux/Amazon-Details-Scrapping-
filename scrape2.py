79abf8ad80161c66d1e5b384573a5730e14ad158
Vaibhav-crux <122672330+Vaibhav-crux@users.noreply.github.com>
2023-03-21 16:45:04 +0530

    Add files via upload
    #In the given url there are 2 methods to be implemented by amazon
                     # i) Required data have a class or id
                     # ii) Required data did not have any class or id
    
    I write both methods.

---
+++ scrape2.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import re
+import requests
+import csv
+
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 OPR/96.0.0.0',
+        'Accept-Language': 'en-US, en;q=0.5'}
+items = []
+with open('amazon_data.csv') as file:
+        csv_reader = csv.DictReader(file)
+
+        for csv_row in csv_reader:
+                url = (csv_row['product url'])
+
+                resp = requests.get(url, headers=HEADERS)
+
+                s = BeautifulSoup(resp.content, features="lxml")
+
+                des = s.select("#productTitle")[0].get_text().strip()
+                details = s.select("#feature-bullets")[0].get_text().strip()
+
+                #In given url there are 2 methods to implemented by amazon
+                 # i) Required data has a class or id
+                 # ii) Required data did not have any class or id
+
+                #i)
+                '''asin = s.select(".a-size-base prodDetAttrValue")[0].get_text().strip()
+                manu = s.select(".a-size-base prodDetAttrValue")[0].get_text().strip()'''
+
+                #ii)
+                #worked when data not having any class or id so it founds the sibling of the data until that data not found
+                manufature = s.find_all('ul', {'class': 'a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list'})[0]
+                asin = manufature.find('span', string=re.compile('ASIN')).find_next_sibling('span').text.strip()
+
+                manu = manufature.find('span', string=re.compile('Manufacturer')).find_next_sibling('span').text.strip()
+                items.append([des,asin,details,manu])
+
+df = pd.DataFrame(items, columns=['Description','ASIN', 'Product Description', 'Manufacturer'])
+df.to_csv("amazon_detail.csv", header=True, index=False)
---
+++ scrip.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from time import sleep
+
+HEADERS= ({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 OPR/96.0.0.0',
+        'Accept-Language': 'en-US, en;q=0.5'})
+
+url = 'https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_1'
+
+items = []
+for i in range(1,11):
+    print('Processing {0}...'.format(url + '&page={0}'.format(i)))
+    response = requests.get(url + '&page={0}'.format(i), headers=HEADERS)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    ans = soup.find_all('div', {'class': 's-result-item','data-component-type':'s-search-result'})
+
+    for k in ans:
+        product_name = k.h2.text
+
+        try:
+            rating = k.find('i',{'class':'a-icon'}).text
+            reviews =k.find('span', {'class':'a-size-base s-underline-text'}).text
+
+        except AttributeError:
+            continue
+
+        try:
+            price = k.find('span', {'class':'a-price-whole'}).text
+            product_url = 'https://www.amazon.in' + k.h2.a['href']
+            # items.append([product_name,rating,reviews,price,product_url])
+            items.append([product_url,product_name,price,rating,reviews])
+        except AttributeError:
+            continue
+
+    sleep(1.5)
+
+df = pd.DataFrame(items, columns=[' Product URL', 'Product Name', 'Product Price', 'Rating', 'Number of reviews'])
+df.to_csv("amazon_data.csv", header=True, index=False)
+
+
